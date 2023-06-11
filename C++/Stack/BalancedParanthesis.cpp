12f9ae6d72ece46c32510444e45450b2d0b67acd
Bhavya Garg <bebhavyagarg@gmail.com>
2023-06-11 14:20:28 +0530

    Wrote code for balanced paranthesis

--- C++/Stack/BalancedParanthesis.cpp
+++ C++/Stack/BalancedParanthesis.cpp
@@ -1,9 +1,30 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
 int main()
 {
-    
+    stack<char> st;
+    string str;
+    cout << "Enter the string \n";
+    cin >> str;
+
+    for (auto &el : str)
+    {
+        if (el == '(' || el == '{' || el == '[')
+        {
+            st.push(el);
+        }
+        else if ((el == ')' && st.top() == '(') || (el == '}' && st.top() == '{') || (el == ']' && st.top() == '['))
+        {
+            st.pop();
+        }
+    }
+
+    if (st.empty())
+        cout << str << " is balanced \n";
+    else
+        cout << str << " is not balanced \n";
+
     return 0;
 }
