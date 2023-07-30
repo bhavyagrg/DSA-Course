91b537bf59136d4e125a38df68f60b6956591d4b
Bhavya Garg <bebhavyagarg@gmail.com>
2023-07-30 13:16:20 +0530

    added delete last node of linked list

--- C++/LinkedList/DeleteEndNode.cpp
+++ C++/LinkedList/DeleteEndNode.cpp
@@ -20,13 +20,36 @@ public:
     }
 };
 
-void printList(Node* head)
+void printList(Node *head)
 {
-    while(head!=NULL)
+    while (head != NULL)
     {
-        cout<<head->value<<" ";
+        cout << head->value << " ";
         head = head->next;
     }
+    cout << endl;
+}
+
+void deleteEnd(Node *&head)
+{
+    if (head == NULL)
+        return;
+    if (head->next == NULL)
+    {
+        delete head;
+        return;
+    }
+
+    Node* curr = head;
+    while(curr->next->next!= NULL)
+    {
+        curr=curr->next;
+    }
+
+    delete curr->next;
+    curr->next = NULL;
+    return;
+    
 }
 
 int main()
@@ -39,6 +62,10 @@ int main()
     Node *n3 = new Node(40);
     n2->next = n3;
 
+    printList(root);
+
+    deleteEnd(root);
+    
     printList(root);
 
     return 0;
